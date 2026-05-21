21c7cae2cd657b71b5de6215246b83c9184bdb1f
qwerty0121 <sh1ng0718o@gmail.com>
2026-05-21 15:39:49 +0000

    第2問：条件分岐の「Rubyらしさ」を意識したうるう年判定の回答に対するレビュー指摘対応

--- 課題/課題2/main.rb
+++ 課題/課題2/main.rb
@@ -1,17 +1,11 @@
 # 第2問：条件分岐の「Rubyらしさ」を意識したうるう年判定
 
 # 西暦がうるう年であるかどうかを判定する
-def leap_year?(year)
-  # 4で割り切れない年はうるう年ではない
-  return false unless year % 4 == 0
+def leap_year?(y)
+  year = y.to_i
 
-  # 400で割り切れる場合はうるう年
-  return true if year % 400 == 0
-
-  # 100で割り切れる年はうるう年ではない
-  return false if year % 100 == 0
-
-  true
+  # 「4で割り切れる」かつ「100で割り切れない」、または「400で割り切れる」
+  (year % 4).zero? && !(year % 100).zero? || (year % 400).zero?
 end
 
 # ケース1
