6ca27f2dfa0a8296248aca9eaf137f812185c71a
qwerty0121 <sh1ng0718o@gmail.com>
2026-05-22 14:22:04 +0000

    第3問：文字列操作と多重代入によるURLパラメーター解析の回答

---
+++ 課題/課題3/main.rb
@@ -0,0 +1,18 @@
+# 第3問：文字列操作と多重代入によるURLパラメーター解析
+
+# URLのクエリ文字列を解析し、パラメータをハッシュとして取得する
+def parse_url_params(params_str)
+  params_str.to_s.split("&").map do |token|
+    key, val = token.split("=")
+    [key, val.to_s]
+  end.to_h
+end
+
+# ケース1
+puts parse_url_params("page=1&sort=desc")
+
+# ケース2
+puts parse_url_params("status=active")
+
+# ケース3
+puts parse_url_params("q=ruby&filter=")
