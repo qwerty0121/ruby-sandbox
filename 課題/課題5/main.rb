b5599489d91cf97c1726c1940c7c7d919381ebe4
qwerty0121 <sh1ng0718o@gmail.com>
2026-05-22 15:43:36 +0000

    第5問：破壊的メソッドと非破壊的メソッドの挙動理解の回答に対するレビュー指摘対応

--- 課題/課題5/main.rb
+++ 課題/課題5/main.rb
@@ -1,26 +1,34 @@
 # 第5問：破壊的メソッドと非破壊的メソッドの挙動理解
 
 # 配列の文字列の前後の空白・改行を除去した上で重複した要素を除去する ※非破壊版
-def exclude_duplicate_with_trim(str_array)
-  str_array.map(&:strip).uniq
+def sanitize(str_array)
+  Array(str_array)
+    .compact
+    .map { |str| str.to_s.strip }
+    .uniq
 end
 
 # 配列の文字列の前後の空白・改行を除去した上で重複した要素を除去する ※破壊版
-def exclude_duplicate_with_trim!(str_array)
-  str_array.map!(&:strip).uniq!
+def sanitize!(str_array)
+  str_array.each { |str| str.to_s.strip! }
+  str_array.uniq! || str_array
 end
 
 arr = [" ruby \n", "java", " ruby", "python "]
 puts arr.object_id
 
 # ケース1
-result1 = exclude_duplicate_with_trim(arr)
+result1 = sanitize(arr)
 puts result1.to_s
 puts result1.object_id
 puts result1.object_id == arr.object_id
 
 # ケース2
-result2 = exclude_duplicate_with_trim!(arr)
+result2 = sanitize!(arr)
 puts result2.to_s
 puts result2.object_id
 puts result2.object_id == arr.object_id
+
+# ケース3: 重複なしの場合
+result3 = sanitize!(["ruby", "java"])
+puts result3.to_s
