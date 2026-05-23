a7ce060cabdf3cb87e36e2987d7568066ed77547
qwerty0121 <sh1ng0718o@gmail.com>
2026-05-23 15:07:08 +0000

    第8問：例外処理（内部例外の捕捉と再発生）の回答に対するレビュー指摘対応

--- 課題/課題8/main.rb
+++ 課題/課題8/main.rb
@@ -1,14 +1,14 @@
 # 第8問：例外処理（内部例外の捕捉と再発生）
 
 def safe_divide(str_a, str_b)
-  begin
-    num_a, num_b = Integer(str_a), Integer(str_b)
-    num_a / num_b
-  rescue ZeroDivisionError
-    Float::INFINITY
-  rescue ArgumentError
-    raise "Invalid Number"
-  end
+  num_a = Integer(str_a)
+  num_b = Integer(str_b)
+
+  num_a / num_b
+rescue ZeroDivisionError
+  Float::INFINITY
+rescue ArgumentError, TypeError
+  raise RuntimeError, "Invalid Number"
 end
 
 # ケース1
@@ -20,6 +20,13 @@ puts safe_divide("10", "0")
 # ケース3
 begin
   puts safe_divide("10", "xyz")
+rescue RuntimeError => e
+  puts "期待通りエラーが発生しました: #{e.class}, #{e.message}"
+end
+
+# その他
+begin
+  puts safe_divide("10", nil)
 rescue RuntimeError => e
   puts "期待通りエラーが発生しました: #{e.class}, #{e.message}"
 end
