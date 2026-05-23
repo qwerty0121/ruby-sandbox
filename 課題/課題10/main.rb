b1fa7dbab61722b585f856c3356e418126f2244a
qwerty0121 <sh1ng0718o@gmail.com>
2026-05-23 15:50:06 +0000

    第10問：Mix-in（モジュールの組み込み）による共通機能の拡張の回答

---
+++ 課題/課題10/main.rb
@@ -0,0 +1,40 @@
+# 第10問：Mix-in（モジュールの組み込み）による共通機能の拡張
+
+module Serializable
+  def to_json_string
+    self.instance_variables.to_h { |v| [v, self.instance_variable_get(v)] }
+  end
+end
+
+class Product
+  include Serializable
+
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+
+class Log
+  include Serializable
+
+  attr_reader :msg, :level
+
+  def initialize(msg, level)
+    @msg = msg
+    @level = level
+  end
+end
+
+# ケース1
+p = Product.new("Apple", 100)
+puts p.to_json_string
+
+# ケース2
+l = Log.new("Login success", "INFO")
+puts l.to_json_string
+
+# ケース3
+puts Product.include?(Serializable)
